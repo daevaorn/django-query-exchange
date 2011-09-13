93ee5e3730277a7b6752bc0a24add874aab1891e
Mikhail Korobov <kmike84@gmail.com>
2011-09-14 01:52:51 +0600

    'remove' support

--- query_exchange/__init__.py
+++ query_exchange/__init__.py
@@ -4,16 +4,16 @@ from django.utils.itercompat import is_iterable
 
 
 def reverse_with_query(viewname, urlconf=None, args=None, kwargs=None, prefix=None, current_app=None,
-                       params=None, keep=None, exclude=None, add=None):
+                       params=None, keep=None, exclude=None, add=None, remove=None):
     url = reverse(viewname, urlconf, args, kwargs, prefix, current_app)
 
     if params is not None:
-        url += '?' + process_query(params, keep, exclude, add)
+        url += '?' + process_query(params, keep, exclude, add, remove)
 
     return url
 
 
-def process_query(params, keep=None, exclude=None, add=None):
+def process_query(params, keep=None, exclude=None, add=None, remove=None):
     data = dict(_extract_items(params))
 
     keep = keep or []
@@ -33,6 +33,15 @@ def process_query(params, keep=None, exclude=None, add=None):
                 data[k].extend(v)
             else:
                 data[k] = v
+
+    if remove:
+        remove = dict(_extract_items(remove))
+        for k, v in remove.iteritems():
+            if k in data:
+                for value in v:
+                    if value in data[k]:
+                        data[k].remove(value)
+
 
     return urlencode([(k, v) for k, l in sorted(data.iteritems()) for v in l])
 
